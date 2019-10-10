dd8863d261a4975ea7d9c504fb14774160d4a8bf
arthur <arthur.teisseire@epitech.eu>
2019-10-10 14:03:19 +0200

    Add FlexTest

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Hellooo from './app/HelloName'
+import FlexTest from "./app/FlexTest";
 
 export default class App extends Component {
     render() {
         return (
+            <View style={{flex: 1}}>
+                <FlexTest style={{flex: 1}}/>
 
-            <View style = {styles.container}>
-                <Text>ABC</Text>
-                <Hellooo name='Pierre'/>
-                <Hellooo name='Maurice'/>
-                <Hellooo name='Didier'/>
+                <View style={{flex: 1}}>
+                    <Text>ABC</Text>
+                    <Hellooo name='Pierre'/>
+                    <Hellooo name='Maurice'/>
+                    <Hellooo name='Didier'/>
+                </View>
             </View>
         );
     }
