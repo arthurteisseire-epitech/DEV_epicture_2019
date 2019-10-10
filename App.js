ffafe3cf2e90b4c8b453f41c6865c114933bb618
arthur <arthur.teisseire@epitech.eu>
2019-10-10 16:27:15 +0200

    Add new scene Login that go to flex scene

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Hellooo from './app/HelloName'
 import FlexTest from "./app/FlexTest";
 import Login from "./app/Login";
+import ButtonTest from "./app/ButtonTest";
+import {Router, Scene, Stack} from 'react-native-router-flux'
 
 export default class App extends Component {
     render() {
         return (
             <View style={{flex: 1}}>
-                <Login/>
-                <FlexTest style={{flex: 1}}/>
+                <Router>
+                    <Stack key="root">
+                        <Scene key="login" component={Login} title="Login"/>
+                        <Scene key="flex" component={FlexTest} title="henry"/>
+                        <Scene key="home" component={ButtonTest}/>
+                    </Stack>
+                </Router>
 
-                <View style={{flex: 1}}>
-                    <Text>ABC</Text>
-                    <Hellooo name='Pierre'/>
-                    <Hellooo name='Maurice'/>
-                    <Hellooo name='Didier'/>
-                </View>
+                {/*<Login/>*/}
+                {/*<ButtonTest/>*/}
+                {/*<FlexTest style={{flex: 1}}/>*/}
+
+                {/*<View style={{flex: 1}}>*/}
+                {/*    <Text>ABC</Text>*/}
+                {/*    <Hellooo name='Pierre'/>*/}
+                {/*    <Hellooo name='Maurice'/>*/}
+                {/*    <Hellooo name='Didier'/>*/}
+                {/*</View>*/}
             </View>
         );
     }
--- app/Login.js
+++ app/Login.js
@@ -3,12 +3,18 @@ import {
     View,
     Text,
     TextInput,
+    Button,
 } from 'react-native'
+import { Actions } from 'react-native-router-flux'
 
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {text: ''};
+        this.state = {login: '', password: ''};
+    }
+
+    ValidLogin() {
+        return this.state.login === "toto" && this.state.password === "tata";
     }
 
     render() {
@@ -17,11 +23,20 @@ export default class Login extends Component {
                 <TextInput
                     style={{height: 40}}
                     placeholder="Enter your login"
-                    onSubmitEditing={(t) => this.setState({text: t.nativeEvent.text})}
+                    onSubmitEditing={(t) => this.setState({login: t.nativeEvent.text})}
+                />
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Enter your password"
+                    onSubmitEditing={(t) => this.setState({password: t.nativeEvent.text})}
                 />
                 <Text style={{padding: 10, fontSize: 25}}>
-                    {"Your name : " + this.state.text}
+                    {"Your name : " + this.state.login}
                 </Text>
+                <Text style={{padding: 10, fontSize: 25}}>
+                    {"Your password : " + this.state.password}
+                </Text>
+                <Button title={"Submit"} onPress={this.ValidLogin() ? Actions.flex : null}/>
             </View>
         )
     }
