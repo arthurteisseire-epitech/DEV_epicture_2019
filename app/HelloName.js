896327af673a0e74b79aaefc589089077a5d37f1
arthur <arthur.teisseire@epitech.eu>
2019-10-10 13:33:39 +0200

    Add hello class

--- app/HelloName.js
+++ app/HelloName.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Hellooo from './app/HelloName'
 
-export default class App extends Component {
+export default class Hellooo extends Component {
     render() {
         return (
-
             <View style = {styles.container}>
-                <Text>ABC</Text>
-                <Hellooo name='Pierre'/>
-                <Hellooo name='Maurice'/>
-                <Hellooo name='Didier'/>
+                <Text> Bonjour {this.props.name} !! </Text>
             </View>
-        );
+        )
     }
 }
 
