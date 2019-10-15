e4f517de21af90aaaa796d29b82b026f0712b767
arthur <arthur.teisseire@epitech.eu>
2019-10-15 17:35:20 +0200

    Use AsyncStorage to store session

--- app/Login.js
+++ app/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {WebView} from 'react-native-webview';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class Login extends Component {
     constructor(props) {
@@ -14,23 +15,11 @@ export default class Login extends Component {
         };
     }
 
-    updateUserData() {
-        if (!this.state.loading) {
-            const regex = /[?&]([^=#]+)=([^&#]*)/g;
-            let params = {};
-            let match = regex.exec(this.state.url);
-            while (match) {
-                params[match[1]] = match[2];
-                match = regex.exec(this.state.url);
-            }
-            this.setState({
-                account_id: params.account_id,
-                account_username: params.account_username,
-                refresh_token: params.refresh_token,
-                access_token: params.access_token,
-            }, this.setState({loading: true}));
-        }
-    }
+    // async componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS): void {
+    //     if (prevState.url !== this.state.url) {
+    //         await this.updateUserData();
+    //     }
+    // }
 
     render() {
         this.updateUserData();
@@ -45,5 +34,47 @@ export default class Login extends Component {
                 }}
             />
         )
+    }
+
+    async updateUserData() {
+        if (!this.state.loading) {
+            const params = this.parseUrl(this.state.url);
+            await this.storeSession(JSON.stringify(params));
+        }
+        let session = this.getSession();
+        await session.then((v) => console.log('session : ' + v));
+    }
+
+    async storeSession(sessionData) {
+        try {
+            await AsyncStorage.setItem('session', sessionData);
+        } catch (error) {
+            console.log('error in set' + error);
+        }
+    }
+
+    async getSession() {
+        try {
+            const session = await AsyncStorage.getItem('session');
+            if (session !== null) {
+                return session;
+            } else {
+                console.log('session is null');
+            }
+        } catch (error) {
+            console.log('error in get: ' + error);
+        }
+        return '';
+    }
+
+    parseUrl(url) {
+        const regex = /[?&]([^=#]+)=([^&#]*)/g;
+        let params = {};
+        let match = regex.exec(url);
+        while (match) {
+            params[match[1]] = match[2];
+            match = regex.exec(url);
+        }
+        return params;
     }
 }
