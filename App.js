3ac30ec88e22760ac348f203eb246269cfdf04d7
arthur <arthur.teisseire@epitech.eu>
2019-10-17 09:18:23 +0200

    Remove unused Components

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import Login from "./app/Login";
-import ButtonTest from "./app/ButtonTest";
 import {Router, Scene, Stack} from 'react-native-router-flux'
 import ImgurFeed from "./app/ImgurFeed";
 
@@ -13,7 +12,6 @@ export default class App extends Component {
                     <Stack key="root">
                         <Scene key="login" component={Login} title="Login"/>
                         <Scene key="feed" component={ImgurFeed} title="henry"/>
-                        <Scene key="home" component={ButtonTest}/>
                     </Stack>
                 </Router>
             </View>
