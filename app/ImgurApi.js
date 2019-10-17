fffe5c90efcae9fec784d109da6cce6d83bf369b
arthur <arthur.teisseire@epitech.eu>
2019-10-17 14:53:10 +0200

    Upload is now working, need to add some check

--- app/ImgurApi.js
+++ app/ImgurApi.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
+import Session from "./Session";
 
 
 export default class ImgurApi {
     constructor() {
         this.apiKey = 'a34ccf491aadd2c';
-        this.rootUrl = 'https://api.imgur.com/3/gallery/t/';
     }
 
     get(url) {
-        return axios.get(this.rootUrl + url, {
+        return axios.get('https://api.imgur.com/3/gallery/t/' + url, {
             headers: {
                 'Authorization': 'Client-ID ' + this.apiKey
             }
@@ -17,6 +17,25 @@ export default class ImgurApi {
             return response.data;
         }).catch((error) => {
             console.log('error : ' + error);
+        });
+    }
+
+    upload(imgBytes) {
+        return Session.get().then((session) => {
+            return axios({
+                method: 'POST',
+                url: 'https://api.imgur.com/3/image',
+                headers: {
+                    'Authorization': 'Bearer ' + JSON.parse(session).access_token
+                },
+                data: {
+                    'image': imgBytes,
+                }
+            }).then((response) => {
+                return response.data;
+            }).catch((error) => {
+                console.log('error : ' + error);
+            });
         });
     }
 }
