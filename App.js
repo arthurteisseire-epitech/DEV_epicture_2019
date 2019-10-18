c3c6403ad3a0517ae74cad53ce3ce8e5f1f179f2
Mahe Caurette <mahe.caurette@epitech.eu>
2019-10-18 19:28:36 +0100

    Add TabBar

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import Login from "./app/Login";
-import {Router, Scene, Stack} from 'react-native-router-flux'
+import {Router, Scene, Stack, Tabs} from 'react-native-router-flux'
 import ImgurFeed from "./app/ImgurFeed";
 import Upload from "./app/Upload";
+import TabBar from './app/TabBar';
+/*import NavBar from "app/NavBar"*/
 
 export default class App extends Component {
     render() {
+        console.disableYellowBox = true;
         return (
             <View style={{flex: 1}}>
                 <Router>
-                    <Stack key="root">
-                        <Scene key="login" component={Login} title="Login"/>
-                        <Scene key="upload" component={Upload} title="Upload"/>
-                        <Scene key="feed" component={ImgurFeed} title="Feed"/>
+                    <Stack key={"root"}>
+                      <Scene
+                        key="login"
+                        title="Login"
+                        component={Login}
+                      />
+                        <Tabs
+                          tabBarComponent={TabBar}
+                          key={'tabs'}
+                          hideNavBar>
+                          <Scene
+                            key="upload"
+                            icon='../img/upload'
+                            title="Upload"
+                            component={Upload}
+                            /*navBar={NavBar}*/
+                          />
+                          <Scene
+                            key="feed"
+                            icon='../img/home'
+                            title="Feed"
+                            component={ImgurFeed}
+                            /*navBar={NavBar}*/
+                          />
+                        </Tabs>
                     </Stack>
                 </Router>
             </View>
--- app/ImgurFeed.js
+++ app/ImgurFeed.js
@@ -1,73 +1,76 @@
-import React, {Component} from 'react'
-import {Text, View, FlatList, TextInput, StyleSheet} from 'react-native'
-import ImgurApi from "./ImgurApi";
-import ImgurPost from "./ImgurPost";
+import React, {Component} from 'react';
+import {Text, View, FlatList, TextInput, StyleSheet} from 'react-native';
+import ImgurApi from './ImgurApi';
+import ImgurPost from './ImgurPost';
 
 export default class ImgurFeed extends Component {
-    constructor(props) {
-        super(props);
-        this.api = new ImgurApi;
-        this.state = {
-            jsonPosts: [],
-            loading: true,
-            images: [],
-            feedName: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.api = new ImgurApi;
+    this.state = {
+      jsonPosts: [],
+      loading: true,
+      images: [],
+      feedName: '',
+    };
+  }
 
-    render() {
-        return (
-            <View>
-                <TextInput
-                    style={styles.FeedSearchBar}
-                    placeholder="Look for a feed !"
-                    onSubmitEditing={(t) => this.setState({feedName: t.nativeEvent.text, loading: true})}
-                />
-                {this.state.images}
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{justifyContent: 'space-between'}}>
+        {/*<View style={{flex: 1, width: 500, height: 500, backgroundColor: 'blue'}}>*/}
+          <TextInput
+            style={styles.FeedSearchBar}
+            placeholder="Look for a feed !"
+            onSubmitEditing={(t) => this.setState({feedName: t.nativeEvent.text, loading: true})}
+          />
+          {this.state.images}
+        {/*</View>*/}
+        {/*<NavBar/>*/}
+      </View>
+    );
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.feedName !== this.state.feedName) {
-            this.api.get(this.state.feedName).then((response) => {
-                this.setState({
-                    jsonPosts: response.data.items,
-                    loading: false
-                });
-            }, (error) => {
-                console.log(error);
-            });
-        }
-        if (prevState.loading !== this.state.loading)
-            this.updateImages();
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevState.feedName !== this.state.feedName) {
+      this.api.get(this.state.feedName).then((response) => {
+        this.setState({
+          jsonPosts: response.data.items,
+          loading: false,
+        });
+      }, (error) => {
+        console.log(error);
+      });
     }
+    if (prevState.loading !== this.state.loading)
+      this.updateImages();
+  }
 
-    updateImages() {
-        if (this.state.loading) {
-            this.state.images = (
-                <View style={styles.FeedStyle}>
-                    <Text>Loading images…</Text>
-                </View>
-            )
-        } else {
-            this.state.images = (
-                <FlatList
-                    style = {styles.FeedStyle}
-                    data={this.state.jsonPosts}
-                    renderItem={(jsonPost) => <ImgurPost jsonData={jsonPost.item}/>}
-                />
-            );
-            this.setState({loading: false});
-        }
+  updateImages() {
+    if (this.state.loading) {
+      this.state.images = (
+        <View style={styles.FeedStyle}>
+          <Text>Loading images…</Text>
+        </View>
+      );
+    } else {
+      this.state.images = (
+        <FlatList
+          style={styles.FeedStyle}
+          data={this.state.jsonPosts}
+          renderItem={(jsonPost) => <ImgurPost jsonData={jsonPost.item}/>}
+        />
+      );
+      this.setState({loading: false});
     }
+  }
 };
 
 const styles = StyleSheet.create({
-    FeedStyle: {
-        backgroundColor: '#003247',
-    },
-    FeedSearchBar: {
-        backgroundColor: '#FFFFB8',
-    },
+  FeedStyle: {
+    backgroundColor: '#003247',
+  },
+  FeedSearchBar: {
+    backgroundColor: '#FFFFB8',
+  },
 });
