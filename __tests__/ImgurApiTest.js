16359c7e4ec6d708fee9a1bb338f1d09a1ec07fe
arthur <arthur.teisseire@epitech.eu>
2019-10-19 10:53:01 +0200

    Remove useless async in feed tests

--- __tests__/ImgurApiTest.js
+++ __tests__/ImgurApiTest.js
@@ -3,12 +3,12 @@ import {render, fireEvent} from "react-native-testing-library";
 import wording from "../app/utils/wording"
 import ImgurFeed from "../app/ImgurFeed";
 
-test("Default feed", async () => {
+test("Default feed", () => {
     const page = render(<ImgurFeed/>);
     expect(page.queryByText(wording.loadingImages)).toBeFalsy();
 });
 
-test("Change feed", async () => {
+test("Change feed", () => {
     const page = render(<ImgurFeed/>);
     const feedInput = page.getByPlaceholder(wording.feedPlaceHolder);
     fireEvent(feedInput, 'onSubmitEditing', {"nativeEvent": {"target": 83, "text": "cats"}});
