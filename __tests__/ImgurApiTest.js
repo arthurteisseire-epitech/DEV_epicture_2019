95d8ab4b1011867fd13657dfd93dc6cd3fcaf496
arthur <arthur.teisseire@epitech.eu>
2019-10-19 11:21:25 +0200

    Add Change feed wait loading test

--- __tests__/ImgurApiTest.js
+++ __tests__/ImgurApiTest.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, fireEvent} from "react-native-testing-library";
+import {render, fireEvent, waitForElement} from "react-native-testing-library";
 import wording from "../app/utils/wording"
 import ImgurFeed from "../app/ImgurFeed";
 
@@ -13,4 +13,18 @@ test("Change feed", () => {
     const feedInput = page.getByPlaceholder(wording.feedPlaceHolder);
     fireEvent(feedInput, 'onSubmitEditing', {"nativeEvent": {"target": 83, "text": "cats"}});
     expect(page.queryByText(wording.loadingImages)).toBeTruthy();
+});
+
+test("Change feed wait loading", async () => {
+    const page = render(<ImgurFeed/>);
+    const feedInput = page.getByPlaceholder(wording.feedPlaceHolder);
+    fireEvent(feedInput, 'onSubmitEditing', {"nativeEvent": {"target": 83, "text": "cats"}});
+    const sleep = (milliseconds) => {
+        return new Promise((resolve) => setTimeout(resolve, milliseconds))
+    };
+    await sleep(2000);
+    const success = await waitForElement(() =>
+        page.queryByText(wording.imagesLoaded)
+    );
+    expect(success).toBeTruthy();
 });
