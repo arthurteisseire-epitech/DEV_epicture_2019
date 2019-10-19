9e72bf5e1918c6206592fd91fe20169b4fe8e905
arthur <arthur.teisseire@epitech.eu>
2019-10-19 10:51:49 +0200

    Add Default feed test

--- __tests__/ImgurApiTest.js
+++ __tests__/ImgurApiTest.js
@@ -3,10 +3,14 @@ import {render, fireEvent} from "react-native-testing-library";
 import wording from "../app/utils/wording"
 import ImgurFeed from "../app/ImgurFeed";
 
+test("Default feed", async () => {
+    const page = render(<ImgurFeed/>);
+    expect(page.queryByText(wording.loadingImages)).toBeFalsy();
+});
+
 test("Change feed", async () => {
     const page = render(<ImgurFeed/>);
     const feedInput = page.getByPlaceholder(wording.feedPlaceHolder);
-    expect(page.queryByText(wording.loadingImages)).toBeFalsy();
     fireEvent(feedInput, 'onSubmitEditing', {"nativeEvent": {"target": 83, "text": "cats"}});
     expect(page.queryByText(wording.loadingImages)).toBeTruthy();
 });
