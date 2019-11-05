0f96430dcfeb9c3ad21e04fe8e7e91f325450c2f
arthur <arthur.teisseire@epitech.eu>
2019-11-05 14:41:49 +0100

    Add disconnection

--- app/ImgurProfile.js
+++ app/ImgurProfile.js
@@ -1,70 +1,82 @@
 import React, {Component} from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
 import Session from './Session';
 import ImgurApi from './ImgurApi';
 import ProfilePics from './ProfilePics';
+import Cookie from "react-native-cookie";
+import {Actions} from "react-native-router-flux";
 
 export default class ImgurProfile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      account_name: '',
-      jsonPics: [],
-      loading: true,
-      images: [],
-    };
-  }
-
-  componentDidMount() {
-    Session.get().then((session) => {
-      this.setState({account_name: JSON.parse(session).account_username});
-    });
-    ImgurApi.getAccountPics().then((response) => {
-      this.setState({
-        jsonPics: response.data,
-        loading: false,
-      });
-      this.updateImages();
-    }, (error) => {
-      console.log(error);
-    });
-  }
-
-  updateImages() {
-    if (this.state.loading) {
-      this.setState({
-        text: <Text>Loading images...</Text>,
-      });
-    } else {
-      this.setState({
-        text: <Text>Images are loaded !</Text>,
-      });
-      this.setState({
-        images:
-          <FlatList
-            style={styles.FeedStyle}
-            data={this.state.jsonPics}
-            initialNumToRender={4}
-            windowSize={5}
-            renderItem={(jsonPic) => <ProfilePics jsonData={jsonPic}/>}
-          />,
-      });
-      this.setState({loading: false});
+    constructor(props) {
+        super(props);
+        this.state = {
+            account_name: '',
+            jsonPics: [],
+            loading: true,
+            images: [],
+        };
     }
-  }
 
-  render() {
-    return (
-      <View>
-        <Text style={{textAlign: 'center', padding: 10, fontSize: 24}}> {this.state.account_name}'s pictures : </Text>
-        {this.state.images}
-      </View>
-    );
-  }
+    componentDidMount() {
+        Session.get().then((session) => {
+            this.setState({account_name: JSON.parse(session).account_username});
+        });
+        ImgurApi.getAccountPics().then((response) => {
+            this.setState({
+                jsonPics: response.data,
+                loading: false,
+            });
+            this.updateImages();
+        }, (error) => {
+            console.log(error);
+        });
+    }
+
+    updateImages() {
+        if (this.state.loading) {
+            this.setState({
+                text: <Text>Loading images...</Text>,
+            });
+        } else {
+            this.setState({
+                text: <Text>Images are loaded !</Text>,
+            });
+            this.setState({
+                images:
+                    <FlatList
+                        style={styles.FeedStyle}
+                        data={this.state.jsonPics}
+                        initialNumToRender={4}
+                        windowSize={5}
+                        renderItem={(jsonPic) => <ProfilePics jsonData={jsonPic}/>}
+                    />,
+            });
+            this.setState({loading: false});
+        }
+    }
+
+    disconnection() {
+        Cookie.clear();
+        Session.clear().then(() => Actions.reset('login'));
+    }
+
+    render() {
+        return (
+            <View>
+                <Button
+                    title={"Disconnection"}
+                    onPress={() => this.disconnection()}
+                />
+                <Text style={{textAlign: 'center', padding: 10, fontSize: 24}}> {this.state.account_name}'s pictures
+                    : </Text>
+                {this.state.images}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  FeedStyle: {
-    backgroundColor: '#003247',
-  },
+    FeedStyle: {
+        backgroundColor: '#003247',
+    },
 });
